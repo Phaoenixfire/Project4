f624db2e5dc9cb18a7c9716cf4911ce1d2b23477
Phaoenixfire <andrewrmiller06@gmail.com>
2020-02-18 13:36:06 -0500

    Testing one two

---
+++ src/main/java/com/example/demo/Project4Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project4Application.class, args);
+	
+	System.out.println("Hi nerds");
+	}
+
+}
